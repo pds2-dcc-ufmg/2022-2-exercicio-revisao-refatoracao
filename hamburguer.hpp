7e7012194b1b729d600e6336730d60091a0f4199
Douglas G. Macharet <doug@dcc.ufmg.br>
2022-10-28 07:30:29 -0300

    Exercício de revisão e refatoração.

---
+++ hamburguer.hpp
@@ -0,0 +1,25 @@
+#ifndef HAMBURGUER_H
+#define HAMBURGUER_H
+
+#include "produto.hpp"
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class hamburguer: public produto{
+    public:
+        hamburguer(int q, float valor_unitario, string tipo, bool artesanal): produto(q, valor_unitario), tipo(tipo), artesanal(artesanal){}
+        string tipo;
+        virtual void print() override{
+            cout << "Hamburguer" << endl;
+            cout << "\tQuantidade: " << q << endl;
+            cout << "\tValor Unitario: " << valorUnitario << endl;
+            cout << "\tTipo: " << tipo << endl;
+            if (artesanal) cout << "\tArtesanal" << endl;
+        }
+        bool artesanal;
+
+};
+
+#endif
---
+++ pizza.hpp
@@ -0,0 +1,25 @@
+#ifndef PIZZA_H
+#define PIZZA_H
+
+#include "produto.hpp"
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class pizza: public produto {
+    public:
+     pizza(int q, float valor_unitario, int pedacos, bool borda_rech): produto(q, valor_unitario), pedacos(pedacos), borda_recheada(borda_rech){}
+        int pedacos;
+        virtual string get_sabor()=0;
+        virtual void print() override{
+            cout << "Pizza de " << get_sabor() << endl;
+            cout << "\tQuantidade: " << q << endl;
+            cout << "\tValor Unitario: " << valorUnitario << endl;
+            cout << "\tPedacos: " << pedacos << endl;
+            if (borda_recheada) cout << "\tBorda Recheada" << endl;
+        }
+        bool borda_recheada;
+};
+
+#endif
