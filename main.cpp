a54abb51af989fd07a8d076def627080466ff50c
Doperino <igorrsmz@gmail.com>
2022-11-06 13:46:05 -0300

    Improving code knowledge, facilitating reading and understanding through changes on variable names

--- main.cpp
+++ main.cpp
@@ -10,58 +10,60 @@ using namespace std;
 
 int main() {
 
-    string line;
-    string tipo;
+    string endereco_cliente;
+    string tipo_produto;
 
-    getline(cin, line);
-    Pedido pedidos(line);
+    getline(cin, endereco_cliente);
+    Pedido pedidos(endereco_cliente);
     
 
-    while(cin >> tipo) {
+    while(cin >> tipo_produto) {
 
-        if(tipo == "Pizza") {
+        if(tipo_produto == "Pizza") {
 
-            produto* prod;
+            produto* produto;
             string sabor;
-            int q,p,b_r;            
-            float v;
+            int quantidade, pedacos, borda_recheada;            
+            float valor_unitario;
             
-            cin >> sabor >> p >> b_r >> v >> q;
 
-            if(b_r == 1) {
-                v = v*1.4;
+            cin >> sabor >> pedacos >> borda_recheada >> valor_unitario >> quantidade;
+
+            if(borda_recheada == 1) {
+                float taxa_borda = 1.4;
+                valor_unitario = valor_unitario * taxa_borda;
             }
 
             if (sabor == "Calabresa") {
 
-                prod = new pizza_calabresa(q,v,p,b_r);
-                pedidos.adiciona_produto(prod);
+                produto = new pizza_calabresa(quantidade, valor_unitario, pedacos, borda_recheada);
+                pedidos.adiciona_produto(produto);
             }
 
             else if (sabor == "Marguerita") {
 
-                prod = new pizza_marguerita(q,v,p,b_r);
-                pedidos.adiciona_produto(prod);
+                produto = new pizza_marguerita(quantidade, valor_unitario, pedacos, borda_recheada);
+                pedidos.adiciona_produto(produto);
             }
         }
 
-        else if(tipo == "Hamburguer") {
+        else if(tipo_produto == "Hamburguer") {
 
-            int q, a;
-            float v;
-            produto* prod;
-            string tipo;
+            int quantidade, artesanal;
+            float valor_unitario;
+            produto* produto;
+            string tipo_hamburguer;
             
-            cin >> tipo >> a >> v >> q;
+            cin >> tipo_hamburguer >> artesanal >> valor_unitario >> quantidade;
 
-            if(a == 1){
-                v = 2.0*v;
+            if(artesanal == 1){
+                float taxa_artesanal = 2.0;
+                valor_unitario = valor_unitario * taxa_artesanal;
             }
 
-            prod = new hamburguer(q, v, tipo, a);
-            pedidos.adiciona_produto(prod);
+            produto = new hamburguer(quantidade, valor_unitario, tipo_hamburguer, artesanal);
+            pedidos.adiciona_produto(produto);
         }
-
     }
 
     pedidos.print_resumo();
