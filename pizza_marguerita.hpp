27ef2a22b19bc107d8072f0fd2f9f79934543691
VitorMoreiraR <111029109+VitorMoreiraR@users.noreply.github.com>
2022-11-10 09:42:59 -0300

    Update pizza_marguerita.hpp

--- pizza_marguerita.hpp
+++ pizza_marguerita.hpp
@@ -10,7 +10,7 @@ class Pizza_marguerita: public Pizza {
 
         Pizza_marguerita(int quantidade, float valor_unitario, int pedacos, bool borda_rech): Pizza(quantidade, valor_unitario, pedacos, borda_rech) {}
 
-        virtual string get_sabor() override{
+        virtual string get_sabor() override {
             return "Marguerita";
         }
 
