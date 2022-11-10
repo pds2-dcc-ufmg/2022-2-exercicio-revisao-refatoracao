2784fb7ce0af806288d3d45d0dae6c86d8a35471
VitorMoreiraR <111029109+VitorMoreiraR@users.noreply.github.com>
2022-11-10 09:41:20 -0300

    Update pedido.hpp

--- pedido.hpp
+++ pedido.hpp
@@ -18,25 +18,25 @@ class Pedido {
 
     public:
 
-        Pedido(string endereco): _endereco(endereco){}
+        Pedido(string endereco): _endereco(endereco) {}
 
-        void adiciona_produto(Produto *p){
+        void adiciona_produto(Produto *p) {
             _produtos.push_back(p);
         }
 
-        float calcula_total(){
+        float calcula_total() {
 
             float valor_total=0.0;
 
             list<Produto*>::iterator it;
-            for (it = _produtos.begin(); it != _produtos.end(); it++){
+            for (it = _produtos.begin(); it != _produtos.end(); it++) {
                 valor_total += (*it)->get_valor_unitario()*(*it)->get_quantidade();
             }
 
             return valor_total;
         }
 
-        void print_resumo(){
+        void print_resumo() {
 
             list<Produto*>::iterator it;
 
@@ -44,7 +44,7 @@ class Pedido {
             cout << "Pedido - Endereco: " << _endereco << endl;
             cout << "Valor total: " << calcula_total() << endl;
 
-            for (it = _produtos.begin(); it != _produtos.end(); it++){
+            for (it = _produtos.begin(); it != _produtos.end(); it++) {
                 (*it)->print_info();
             }
             cout << "================================" << endl;
