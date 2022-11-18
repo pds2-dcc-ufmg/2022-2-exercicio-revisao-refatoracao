5caddbc5d56d203b1fefb99c8832de1c32fe741e
Henrique Medeiros <112737578+meisontanker@users.noreply.github.com>
2022-11-17 23:59:59 -0300

    Add files via upload

--- produto.hpp
+++ produto.hpp
@@ -1,11 +1,15 @@
 #ifndef PRODUTO_H
 #define PRODUTO_H
-class produto{
+
+class produto {
     public:
-        int q;
-        produto(int q, float valor_unitario):q(q), valorUnitario(valor_unitario){}
-        virtual void print(){}
+        int quant;
         float valorUnitario;
+
+        produto(int quant, float valor_unitario):quant(quant), valorUnitario(valor_unitario) {}
+        
+        virtual void print() {}
+        
 };
 
 #endif
